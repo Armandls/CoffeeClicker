2e2d7fda433770e2981f84034382c5d2c0f4fc4d
Nil de Creus Massot <nil.decreus@students.salle.url.edu>
2024-03-25 20:10:49 +0100

    S'ha fet el SQLConnector amb el fitxer proporcionat d'exemple a l'estudy, falta canviar el Constructor i el getInstance per adaptar-lo al notre projecte

--- src/Persistance/SQL/SQLConnector.java
+++ src/Persistance/SQL/SQLConnector.java
@@ -1,4 +1,140 @@
 package Persistance.SQL;
 
+import java.sql.*;
+
+/**
+ * The SQLConnector class will abstract the specifics of the connection to a MySQL database.
+ *
+ * This class follows the Singleton design pattern to facilitate outside access while maintaining
+ * a single instance, as having multiple connectors to a database is generally discouraged.
+ *
+ * Be aware that this class presents a simplified approach. Configuration parameters SHOULD NOT be
+ * hardcoded and the use of Statements COULD be replaced by PreparedStatements to avoid SQL Injection.
+ */
 public class SQLConnector {
+
+    // The static attribute to implement the singleton design pattern.
+    private static SQLConnector instance = null;
+
+
+    /**
+     * Static method that returns the shared instance managed by the singleton.
+     *
+     * @return The shared SQLConnector instance.
+     */
+    public static SQLConnector getInstance(){
+        if (instance == null ){
+            // NOT a good practice to hardcode connection data! Be aware of this for your project delivery ;)
+            instance = new SQLConnector("root", "swain", "localhost", 3306, "oopd_studentsdb");
+            instance.connect();
+        }
+        return instance;
+    }
+
+    // Attributes to connect to the database.
+    private final String username;
+    private final String password;
+    private final String url;
+    private Connection conn;
+
+    // Parametrized constructor
+    private SQLConnector(String username, String password, String ip, int port, String database) {
+        this.username = username;
+        this.password = password;
+        this.url = "jdbc:mysql://" + ip + ":" + port + "/" + database;
+    }
+
+
+    /**
+     * Method that starts the inner connection to the database. Ideally, users would disconnect after
+     * using the shared instance.
+     */
+    public void connect() {
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch(SQLException e) {
+            System.err.println("Couldn't connect to --> " + url + " (" + e.getMessage() + ")");
+        }
+    }
+
+
+    /**
+     * Method that executes an insertion query to the connected database.
+     *
+     * @param query String representation of the query to execute.
+     */
+    public void insertQuery(String query){
+        try {
+            Statement s = conn.createStatement();
+            s.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when inserting --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
+
+
+    /**
+     * Method that executes an update query to the connected database.
+     *
+     * @param query String representation of the query to execute.
+     */
+    public void updateQuery(String query){
+        try {
+            Statement s = conn.createStatement();
+            s.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problema when updating --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
+
+
+    /**
+     * Method that executes a deletion query to the connected database.
+     *
+     * @param query String representation of the query to execute.
+     */
+    public void deleteQuery(String query){
+        try {
+            Statement s = conn.createStatement();
+            s.executeUpdate(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when deleting --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+
+    }
+
+
+    /**
+     * Method that executes a selection query to the connected database.
+     *
+     * @param query String representation of the query to execute.
+     * @return The results of the selection.
+     */
+    public ResultSet selectQuery(String query){
+        ResultSet rs = null;
+        try {
+            Statement s = conn.createStatement();
+            rs = s.executeQuery(query);
+        } catch (SQLException e) {
+            System.err.println(query);
+            System.err.println("Problem when selecting data --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+        return rs;
+    }
+
+
+    /**
+     * Method that closes the inner connection to the database. Ideally, users would disconnect after
+     * using the shared instance.
+     */
+    public void disconnect(){
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println("Problem when closing the connection --> " + e.getSQLState() + " (" + e.getMessage() + ")");
+        }
+    }
 }
