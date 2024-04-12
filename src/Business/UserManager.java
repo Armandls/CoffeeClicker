a2c6a5905590a08d1da966b2c02a4047e1b7d699
Nil de Creus Massot <nil.decreus@students.salle.url.edu>
2024-04-12 11:26:32 +0200

    S'ha implementat que el LoginController contingui el UserManager. S'ha creat un constructor de UserManager que funcioni per crear-lo sense passar-li valors per referencia. S'ha implementat la logica del login per buscar la informació possada en els camps de username i password buscar a la base de dades l'usuari.

--- src/Business/UserManager.java
+++ src/Business/UserManager.java
@@ -2,6 +2,7 @@ package Business;
 
 import Business.Entities.User;
 import Persistance.DAO.UserDAO;
+import Persistance.SQL.SQLUserDAO;
 
 /*Class to manage the different entities regarding the Game
  *  @User
@@ -12,6 +13,30 @@ public class UserManager {
 
     public UserManager(UserDAO userDAO) {
         this.userDAO = userDAO;
+    }
+    public UserManager() {
+        this.userDAO = new SQLUserDAO();
+    }
+
+    /**
+     * Funció que retorna la informació del usuari que es vol buscar amb email a la base de dades separada en els seus camps
+     * en un array de String.
+     * @param email es el valor que utilitzem per cercar l'usuari a al base de dades.
+     * @return String[3]:   pos0 -> nickname.
+     *                      pos1 -> email.
+     *                      pos2 -> password
+     *          En el cas de que no trobi l'usuari retornara un null. Aquesta gestió es canviara amb la implementació de les excepcions.
+     */
+    public String[] getUserInfo(String email) {
+        String[] info = new String[3];
+        User user = userDAO.getUser(email);
+        if (user != null) {
+            info[0] = user.getNickname();
+            info[1] = user.getEmail();
+            info[2] = user.getPassword();
+            return info;
+        }
+        return null;
     }
 
     public void addUser(User user) {
--- src/Presentation/Controllers/LoginController.java
+++ src/Presentation/Controllers/LoginController.java
@@ -1,5 +1,6 @@
 package Presentation.Controllers;
 
+import Business.UserManager;
 import Presentation.FrameController;
 import Presentation.MainController;
 import Presentation.Views.LoginView;
@@ -13,11 +14,14 @@ import java.awt.event.KeyListener;
 public class LoginController implements ActionListener {
 
     private FrameController mainController;
+    private UserManager userManager;
 
     private LoginView loginView;
 
     public LoginController(FrameController mainController) {
         this.mainController = mainController;
+        this.userManager = new UserManager();
+
     }
 
     @Override
@@ -49,9 +53,33 @@ public class LoginController implements ActionListener {
     void login() {
         //TODO: Implement login
         String[] info = loginView.getInfo();
+
+        //Dades introduides usuari.
+        String userLoginMail = info[0].split(":")[1].trim();
+        String userLoginPass = info[1].split(":")[1].trim().replaceAll(" ", "").replaceAll(",","").replace("[", "").replace("]","");
+        String userLoginRem =  info[2].split(":")[1].trim();
+
+        //Cerca d'informació
+        String[] userInfo = userManager.getUserInfo(userLoginMail);
+        if (userInfo != null) {
+            if (userInfo[2].equals(userLoginPass)) {
+                //Cas en el que s'ha loguejat correctament
+                mainController.swapPanel("game");
+            }
+            else {
+                //Cas en que la contrassenya no s'ha introduit correctament
+                System.out.println("Contrasenya incorrecta");
+            }
+        }
+        else {
+            //Cas en el que no s'ha trobat l'usuari en la base de dades amb l'email proporcionat.
+            System.out.println("Usuari no trobat");
+        }
+        /*
         if (info[0].split(":")[1].equalsIgnoreCase("admin")) {
             mainController.swapPanel("game");
         }
+        */
     }
 }
 
