dc478082bb7a5c6a9578a3d1bac4a4263aed2011
armand <armand.lopez@students.salle.url.edu>
2024-05-25 14:09:00 +0200

    javadoc complet

--- src/Presentation/FrameController.java
+++ src/Presentation/FrameController.java
@@ -2,8 +2,28 @@ package Presentation;
 
 import Business.Exception.BusinessException;
 
+/**
+ * Interface for controlling frames or panels within the application.
+ */
 public interface FrameController {
 
+    /**
+     * Swaps the current panel with another panel.
+     *
+     * @param panelName The name of the panel to swap to.
+     */
     void swapPanel(String panelName);
+
+    /**
+     * Initializes the game with the provided data.
+     *
+     * @param currency       The initial currency value.
+     * @param basicGenerator The quantity of basic generators.
+     * @param midGenerator   The quantity of mid-tier generators.
+     * @param highGenerator  The quantity of high-tier generators.
+     * @param lvlBasicImp    The level of basic improvements.
+     * @param lvlMidImp      The level of mid-tier improvements.
+     * @param lvlHighImp     The level of high-tier improvements.
+     */
     void initializeGame(int currency, int basicGenerator, int midGenerator, int highGenerator, int lvlBasicImp, int lvlMidImp, int lvlHighImp);
 }
--- src/Presentation/JImagePanel.java
+++ src/Presentation/JImagePanel.java
@@ -7,20 +7,31 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A custom JPanel that displays an image.
+ */
 public class JImagePanel extends JPanel {
 
     private BufferedImage image;
     private int res;
-
     private float alpha;
 
     public static final int EXTEND_RES_WIDTH = 1;
+
+    /**
+     * Constructs a new JImagePanel.
+     */
     public JImagePanel() {
         super();
         setOpaque(false);
         res = 0;
     }
 
+    /**
+     * Constructs a new JImagePanel with the specified image.
+     *
+     * @param image The image to be displayed.
+     */
     public JImagePanel(BufferedImage image) {
         super();
         this.alpha = 1.0f;
@@ -28,24 +39,53 @@ public class JImagePanel extends JPanel {
         setOpaque(false);
     }
 
-
+    /**
+     * Constructs a new JImagePanel with the image loaded from the specified path.
+     *
+     * @param path The path to the image file.
+     * @throws IOException if the image cannot be loaded.
+     */
     public JImagePanel(String path) throws IOException {
         this.alpha = 1.0f;
         setOpaque(false);
         this.image = loadImage(path);
     }
 
-
+    /**
+     * Sets the image to be displayed.
+     *
+     * @param image The image to be displayed.
+     */
     public void setImage(BufferedImage image) {
         this.image = image;
         this.alpha = 1.0f;
         repaint();
     }
 
-    public void setResolution (int res) {
+    /**
+     * Sets the resolution mode of the image.
+     *
+     * @param res The resolution mode. Use {@link #EXTEND_RES_WIDTH} for extending the image to the width of the panel.
+     */
+    public void setResolution(int res) {
         this.res = res;
     }
 
+    /**
+     * Sets the transparency of the image.
+     *
+     * @param alpha The alpha value (transparency) of the image.
+     */
+    public void setAlpha(float alpha) {
+        this.alpha = alpha;
+        revalidate();
+        repaint();
+    }
+
+    /**
+     * Paints the image on the panel.
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
@@ -79,6 +119,27 @@ public class JImagePanel extends JPanel {
         g2d.dispose();
     }
 
+    /**
+     *  Returns the preferred size of the panel.
+     * @return
+     */
+    @Override
+    public Dimension getPreferredSize() {
+        if (image != null) {
+            return new Dimension(image.getWidth(), image.getHeight());
+        } else {
+            return super.getPreferredSize();
+        }
+    }
+
+    /**
+     * Draws the image centered on the panel.
+     * @param g2d
+     * @param panelWidth
+     * @param panelHeight
+     * @param imageWidth
+     * @param imageHeight
+     */
     private void drawCenteredImage(Graphics2D g2d, int panelWidth, int panelHeight, int imageWidth, int imageHeight) {
         double scaleFactor = Math.min((double) panelWidth / imageWidth, (double) panelHeight / imageHeight);
         int newImageWidth = (int) (imageWidth * scaleFactor);
@@ -88,6 +149,14 @@ public class JImagePanel extends JPanel {
         g2d.drawImage(image, xOffset, yOffset, newImageWidth, newImageHeight, this);
     }
 
+    /**
+     * Draws the image stretched to fit the panel.
+     * @param g2d
+     * @param panelWidth
+     * @param panelHeight
+     * @param imageWidth
+     * @param imageHeight
+     */
     private void drawStretchedImage(Graphics2D g2d, int panelWidth, int panelHeight, int imageWidth, int imageHeight) {
         double scaleFactor = (double) panelWidth / imageWidth;
         int newImageWidth = panelWidth;
@@ -97,6 +166,14 @@ public class JImagePanel extends JPanel {
         g2d.drawImage(image, xOffset, yOffset, newImageWidth, newImageHeight, this);
     }
 
+    /**
+     * Draws the image stretched to fit the panel with an offset.
+     * @param g2d
+     * @param panelWidth
+     * @param panelHeight
+     * @param imageWidth
+     * @param imageHeight
+     */
     private void drawOffsetStretchedImage(Graphics2D g2d, int panelWidth, int panelHeight, int imageWidth, int imageHeight) {
         double scaleFactor = (double) panelWidth / imageWidth;
         int newImageWidth = panelWidth;
@@ -108,23 +185,13 @@ public class JImagePanel extends JPanel {
         g2d.drawImage(image, xOffset, yOffset, adjustedWidth, adjustedHeight, this);
     }
 
-
-    public void setAlpha(float alpha) {
-        this.alpha = alpha;
-        revalidate();
-        repaint();
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        if (image != null) {
-            return new Dimension(image.getWidth(), image.getHeight());
-        } else {
-            return super.getPreferredSize();
-        }
-    }
-
-    public static BufferedImage loadImage(String imagePath) throws IOException {
+    /**
+     * Loads an image from the specified path.
+     * @param imagePath
+     * @return
+     * @throws IOException
+     */
+    private static BufferedImage loadImage(String imagePath) throws IOException {
         try {
             File file = new File(System.getProperty("user.dir") + imagePath);
             return ImageIO.read(file);
