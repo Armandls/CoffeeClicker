58342a1f8ff2e4951dea7019f6aabc915ecaf6a0
Aleix Batchelli I Abad <abatchelli12@gmail.com>
2024-05-26 18:29:21 +0200

    --

--- src/Presentation/Controllers/StatisticsController.java
+++ src/Presentation/Controllers/StatisticsController.java
@@ -1,5 +1,6 @@
 package Presentation.Controllers;
 
+import Persistance.Exception.ConnectionErrorException;
 import Presentation.Interfaces.StatisticsControllerI;
 
 import java.awt.event.ActionEvent;
@@ -33,13 +34,38 @@ public class StatisticsController implements ActionListener {
                 mainController.swapPanel("game");
                 break;
             case "logout":
+                logout();
+                mainController.logout();
                 break;
             case "deleteAccount":
+                try {
+                    deleteAccount();
+                } catch (ConnectionErrorException ex) {
+                    throw new RuntimeException(ex);
+                }
+                mainController.swapPanel("login");
                 break;
             default:
                 String gameId = e.getActionCommand();
                 mainController.displayGameStats(gameId);
                 break;
         }
+    }
+
+    /**
+     * Logs out the current user.
+     */
+    void logout() {
+        mainController.restartValuesUser();
+        mainController.restartGame();
+    }
+
+    /**
+     * Deletes the current user's account.
+     *
+     * @throws ConnectionErrorException If there is an error connecting to the database.
+     */
+    void deleteAccount() throws ConnectionErrorException {
+        mainController.deleteUser();
     }
 }
--- src/Presentation/Interfaces/StatisticsControllerI.java
+++ src/Presentation/Interfaces/StatisticsControllerI.java
@@ -1,5 +1,8 @@
 package Presentation.Interfaces;
 
+import Persistance.Exception.ConnectionErrorException;
+import Persistance.Exception.PersistenceException;
+
 /**
  * Interface for StatisticsController.
  */
@@ -17,4 +20,26 @@ public interface StatisticsControllerI {
      * @param id The ID of the game to display statistics for.
      */
     void displayGameStats(String id);
+
+    /**
+     * Logs out the user.
+     */
+    void logout();
+
+    /**
+     * Deletes the user account.
+     *
+     * @throws ConnectionErrorException if a connection error occurs.
+     */
+    void deleteUser() throws ConnectionErrorException;
+
+    /**
+     * Restarts the game.
+     */
+    void restartGame();
+
+    /**
+     * Restarts the user values.
+     */
+    void restartValuesUser();
 }
